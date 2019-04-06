b44b5dc1db86f3e7e495fea9aafb9c9e937c53fd
armani <razzarioa@gmail.com>
2019-04-06 12:59:31 +0600

    Added goods to models, created method str for models

--- exam/webapp/models.py
+++ exam/webapp/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=2000, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Good(models.Model):
     name = models.CharField(max_length=255)
@@ -14,10 +17,16 @@ class Good(models.Model):
     categories = models.ManyToManyField(Category, related_name='good')
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return 'Good is {0}, arrival date: {1}'.format(self.name, self.arrival_date)
+
 
 class Photo(models.Model):
     good = models.ForeignKey(Good, on_delete=models.CASCADE, related_name='good_photo')
     photo = models.ImageField(upload_to='posters')
+
+    def __str__(self):
+        return str(self.good)
 
 
 class Order(models.Model):
@@ -27,3 +36,6 @@ class Order(models.Model):
     address = models.TextField(max_length=2000, null=True, blank=True)
     comments = models.TextField(max_length=2000, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{0}, address is {1}'.format(self.user, self.address)
