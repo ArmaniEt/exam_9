b96f5ac92ad13a6bf5572b8779546317967a5ad2
armani <razzarioa@gmail.com>
2019-04-06 12:28:18 +0600

    Registered models in admin.py

--- exam/webapp/admin.py
+++ exam/webapp/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from webapp.models import Order, Category, Good, Photo
 
-# class MovieAdmin(admin.ModelAdmin):
-#     list_display = ['pk', 'name', 'release_date']
-#     ordering = ['-release_date']
-#     search_fields = ['name', 'id']
-#
-#
-# admin.site.register(Movie, MovieAdmin)
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'arrival_date']
+    filter_horizontal = ['categories']
+    ordering = ['-arrival_date']
+    search_fields = ['name', 'id']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'created_at']
+    ordering = ['-created_at']
+    search_fields = ['user', 'id']
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Good, GoodAdmin)
+admin.site.register(Photo)
+admin.site.register(Category)
+
 
